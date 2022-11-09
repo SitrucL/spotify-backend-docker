c356ee0020e6d5ca00f3c18e364caeebf73b82b6
Curtis Campbell <curtis.campbell1@engie.com>
2022-11-09 23:49:57 +0000

    feat starts polling on new socket connection

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,10 @@ import getCurrentlyPlaying from './getCurrentlyPlaying';
 import getUserAuthorisation from './getUserAuthorisation';
 import refreshAccessToken from './refreshAccessToken';
 import { exit } from 'process';
+import process from 'process';
 
 const cached_token_exists = fs.existsSync('initial_token.json');
-
+let isPolling = false;
 const getToken = async () => {
     const data = fs.readFileSync('initial_token.json', 'utf8');
     const parsedData: authenticationResponse = JSON.parse(data);
@@ -23,15 +24,12 @@ const getToken = async () => {
 };
 
 const startPolling = async (token: string) => {
+    isPolling = true;
     const poll_current_track_timer = setInterval(async () => {
         try {
-            const track = await getCurrentlyPlaying(token);
-
-            console.log('emitted: ', {
-                track: track?.currentlyPlaying.name,
-                timestamp: new Date().toLocaleString(),
-            });
-            // TODO: get current track and emit info as socket event
+            const track_data = await getCurrentlyPlaying(token);
+            console.log('emitted track_data: ', track_data?.currentlyPlaying.name);
+            server.emit('track_info', track_data);
         } catch (error) {
             console.log('error getting current track: ', error);
         }
@@ -40,30 +38,26 @@ const startPolling = async (token: string) => {
 };
 
 io.on('connection', (socket) => {
-    //TODO: if not already polling spotify - trigger authentication or token refresh
-    //TODO: begin polling spotify
-    //TODO: emit info over websockets
-});
+    const connectionCount = io.engine.clientsCount;
+    console.log('NEW CLIENT CONNECTION:', { id: socket.id, index: connectionCount });
 
-io.on('disconnect', () => {
-    // TODO: check if all clients have been disconnected and stop polling if true
+    if (!isPolling) {
+        if (!cached_token_exists) {
+            console.log('NEED TO AUTHORISE APP FOR ACCOUNT');
+        }
+
+        if (cached_token_exists) {
+            getToken().then((token) => startPolling(token)); //initiates the polling with existing token
+
+            const refresh_token_timer = setInterval(() => {
+                //grabs a new token  after timer elapses
+                getToken().then((token) => startPolling(token));
+            }, 359999); //refreshes every hour
+            () => clearInterval(refresh_token_timer);
+        }
+    }
 });
 
 server.listen(3000, () => {
     console.log('SOCKET INITIATION SERVER LISTENING ON PORT 3000');
 });
-
-if (!cached_token_exists) {
-    console.log('NEED TO AUTHORISE APP FOR ACCOUNT');
-    exit();
-}
-
-if (cached_token_exists) {
-    getToken().then((token) => startPolling(token)); //initiates the polling with existing token
-
-    const refresh_token_timer = setInterval(() => {
-        //grabs a new token  after timer elapses
-        getToken().then((token) => startPolling(token));
-    }, 359999); //refreshes every hour
-    () => clearInterval(refresh_token_timer);
-}
