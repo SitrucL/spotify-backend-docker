4693e41b3f09778e0a90ad7f83842a94f825c1f6
Curtis Campbell <curtis.campbell1@engie.com>
2022-11-10 09:25:04 +0000

    feat: adds keylogger for disconnecting to test socket client

--- src/test-socket-client.ts
+++ src/test-socket-client.ts
@@ -1,7 +1,25 @@
 import { io } from 'socket.io-client';
 
+import readline from 'readline';
 const socket = io('http://localhost:3000');
-// socket.connect();
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', (str, key) => {
+    if (key.name === 'd') {
+        socket.close();
+        process.exit();
+    } else if (key.name === 'r') {
+        socket.connect();
+    } else {
+        console.log(`You pressed the "${str}" key`);
+        console.log();
+        console.log(key);
+        console.log();
+    }
+});
+
 socket.on('connect', () => {
     console.log('CONNECTED TO THE SOCKET SERVER');
 });
