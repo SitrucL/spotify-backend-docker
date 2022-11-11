afb44de79bffff65c2cf6411dbe640f9525090f7
Curtis Campbell <curtis.campbell1@engie.com>
2022-11-11 08:59:09 +0000

    chore: adds docker file and changes port to 8000

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,6 @@ io.on('connection', (socket) => {
     }
 });
 
-server.listen(3000, () => {
-    console.log('SOCKET INITIATION SERVER LISTENING ON PORT 3000');
+server.listen(8000, () => {
+    console.log('SOCKET INITIATION SERVER LISTENING ON PORT 8000');
 });
